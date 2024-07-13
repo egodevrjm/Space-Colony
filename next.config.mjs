65c860541bcc365cf54428a79c8e4888d1102fc4
Ryan Morrison <ryanmorrison@gmail.com>
2024-07-13 23:54:25 +0100

    name change

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-    poweredByHeader: false,
-    async headers() {
-      return [
-        {
-          source: '/(.*)',
-          headers: [
-            {
-              key: 'X-Frame-Options',
-              value: 'DENY',
-            },
-          ],
-        },
-      ]
-    },
-    // Add any other configurations you need here
-  };
-  
-  module.exports = nextConfig;
+const nextConfig = {};
+
+export default nextConfig;
