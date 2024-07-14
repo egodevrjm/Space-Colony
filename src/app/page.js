6bf094b6969496352300e01abf58691ee5724f77
Ryan Morrison <ryanmorrison@gmail.com>
2024-07-14 10:05:40 +0100

    tutorial and other updatee to UI

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,24 @@
 'use client'
 
+import { useState, useEffect } from 'react';
 import ExodusGame from '../components/ExodusGame'
 
 export default function Home() {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null; // or a loading spinner
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <ExodusGame />
+    <main className="flex min-h-screen flex-col items-center justify-start p-4 sm:p-6 md:p-8 lg:p-12 overflow-x-hidden">
+      <div className="w-full max-w-7xl">
+        <ExodusGame />
+      </div>
     </main>
   )
 }
